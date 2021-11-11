8b094e4e499da09e5930c2d11258ebf253f29e9a
Sarthik Garg <sarthikgarg01@gmail.com>
2021-11-12 03:09:40 +0530

    added Population and Sample radio buttons

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ const output = document.getElementById("output");
 variance.addEventListener("click", () => {
 	const value = input.value.split(" ").map((e) => parseInt(e));
 	const mean = value.reduce((a, c) => a + c) / value.length;
-	output.innerText = value.reduce((a, c) => a + Math.pow(mean - c, 2), 0) / (value.length - 1);
+	const deno = document.querySelector('input[name="denominator"]:checked').value === "Sample" ? 1 : 0;
+	output.innerText = value.reduce((a, c) => a + Math.pow(mean - c, 2), 0) / (value.length - deno);
 });
 
 square.addEventListener("click", () => {
